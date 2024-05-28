51e073aef271844aa9ee2277b937bf991c3bcd76
OzanSentrk <ozan.senturk@bilgiedu.net>
2024-05-29 00:16:34 +0300

    progress bar
    progress barlar çalışır durumda. Algoritma için pythona kullanıcı alınan ve alınması gereken kalori miktarları gönderilir durumda.

--- desktop-1.php
+++ desktop-1.php
@@ -27,6 +27,9 @@ session_start();
       <div class="auto-group-ur4c-ukx">
         <p class="item-1150-BiU">
           <span class="item-1150-BiU-sub-1" id="dailyCalories"></span>
+          <div class="calories-progress-container">
+          <div class="calories-progress-bar" id="caloriesProgress"></div>
+        </div>
         </p>
         <p class="kalan-WGt">
           <span class="kalan-WGt-sub-0" id="dailyCaloriesValue"></span>
@@ -40,15 +43,22 @@ session_start();
       </div>
       <div class="auto-group-8vrg-w6c">
         <p class="g-tGk" id="dailyCarbohydrate"></p>
-        <div class="progress-container">
-          <div class="progress-bar" id="carbohydrateProgress"></div>
+        <div class="Carb-progress-container">
+          <div class="Carb-progress-bar" id="carbohydrateProgress"></div>
         </div>
         <p class="g-o8p" id="dailyProtein"></p>
+        <div class="protein-progress-container">
+          <div class="protein-progress-bar" id="proteinProgress"></div>
+        </div>
         <p class="g-JLU" id="dailyFat"></p>
+        <div class="fat-progress-container">
+          <div class="fat-progress-bar" id="fatProgress"></div>
+        </div>
+        
       </div>
     </div>
   </div>
-  <div class="auto-group-mkma-jwa clickable-icon" onclick="navigateToFrame3()">
+  <div class="auto-group-mkma-jwa clickable-icon" onclick="getUserInfo()" onclick="navigateToFrame3()">
     <div class="auto-group-ts56-rmJ">
       <img class="logout-3-AX6" src="./assets/logout-3.png"/>
       <p class="k-yap-RC8">Çıkış Yap</p>
@@ -90,6 +100,8 @@ session_start();
 </div>
 
 <script>
+  var dailyNeeds;
+
   function navigateToDesktop3() {
     window.location.href = "desktop-3.html";
   }
@@ -98,9 +110,12 @@ session_start();
     window.location.href = "frame-3.html";
   }
 
-  document.addEventListener('DOMContentLoaded', function() {
+  var dailyNeeds;
+var dailyValues;
+
+document.addEventListener('DOMContentLoaded', function() {
     // Oturumdaki değerleri JavaScript'e alın
-    var dailyNeeds = {
+    dailyNeeds = {
       calories: "<?php echo $_SESSION['calorie']; ?>",
       carbohydrate: "<?php echo $_SESSION['carbohydrate']; ?>",
       protein: "<?php echo $_SESSION['protein']; ?>",
@@ -115,28 +130,82 @@ session_start();
     document.getElementById('dailyCarbohydrate').innerHTML += "Günlük Karbonhidrat İhtiyacı: " + dailyNeeds.carbohydrate + " g";
     document.getElementById('dailyProtein').innerHTML += "Günlük Protein İhtiyacı: " + dailyNeeds.protein + " g";
     document.getElementById('dailyFat').innerHTML += "Günlük Yağ İhtiyacı: " + dailyNeeds.fat + " g";
-  });
 
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var dailyValues = JSON.parse(xhr.responseText);
-      console.log("Günlük Değerler: " + JSON.stringify(dailyValues));
+    // dailyValues için XMLHttpRequest ile ayrı bir istek yapın
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        dailyValues = JSON.parse(xhr.responseText);
+        console.log("Günlük Değerler: " + JSON.stringify(dailyValues));
+        
+        // Günlük değerleri HTML içindeki uygun yerlere yerleştirme
+        document.getElementById('dailyCalories').innerHTML += "<br>Alınan: " + dailyValues.calories + " kcal";
+        document.getElementById('dailyProtein').innerHTML += "<br>Alınan: " + dailyValues.protein + " g";
+        document.getElementById('dailyFat').innerHTML += "<br>Alınan: " + dailyValues.fat + " g";
+        document.getElementById('dailyCarbohydrate').innerHTML += "<br>Alınan: " + dailyValues.carbohydrate + " g";
       
-      // Günlük değerleri HTML içindeki uygun yerlere yerleştirme
-      document.getElementById('dailyCalories').innerHTML += "<br>Alınan: " + dailyValues.calories + " kcal";
-      document.getElementById('dailyProtein').innerHTML += "<br>Alınan: " + dailyValues.protein + " g";
-      document.getElementById('dailyFat').innerHTML += "<br>Alınan: " + dailyValues.fat + " g";
-      document.getElementById('dailyCarbohydrate').innerHTML += "<br>Alınan: " + dailyValues.carbohydrate + " g";
-    
-    
-      var carbProgress = (dailyValues.carbohydrate / dailyNeeds.carbohydrate) * 100;
-      document.getElementById('carbohydrateProgress').style.width = carbProgress + '%';
+        // Progress barı güncelleme
+        var carbProgress = (dailyValues.carbohydrate / dailyNeeds.carbohydrate) * 100;
+        if (carbProgress >= 100) {
+          document.getElementById('carbohydrateProgress').style.width = '100%';
+          document.getElementById('carbohydrateProgress').style.backgroundColor = '#EA3636'; // Set progress bar color to red
+        } else {
+          document.getElementById('carbohydrateProgress').style.width = carbProgress + '%';
+        }
+          
+        var proteinProgress = (dailyValues.protein / dailyNeeds.protein) * 100;
+        if (proteinProgress >= 100) {
+          document.getElementById('proteinProgress').style.width = '100%';
+          document.getElementById('proteinProgress').style.backgroundColor = '#EA3636'; // Set progress bar color to red
+        } else {
+          document.getElementById('proteinProgress').style.width = proteinProgress + '%';
+        }
 
-    }
-  };
-  xhr.open("GET", "dailyIntake.php", true);
-  xhr.send();
+        var fatProgress = (dailyValues.fat / dailyNeeds.fat) * 100;
+        if (fatProgress >= 100) {
+          document.getElementById('fatProgress').style.width = '100%';
+          document.getElementById('fatProgress').style.backgroundColor = '#EA3636'; // Set progress bar color to red
+        } else {
+          document.getElementById('fatProgress').style.width = fatProgress + '%';
+        }
+          
+        var caloriesProgress = (dailyValues.calories / dailyNeeds.calories) * 100;
+        if (caloriesProgress >= 100) {
+          document.getElementById('caloriesProgress').style.width = '100%';
+          document.getElementById('caloriesProgress').style.backgroundColor = '#EA3636'; // Set progress bar color to red
+        } else {
+          document.getElementById('caloriesProgress').style.width = caloriesProgress + '%';
+        }
+
+        // Hem dailyNeeds hem de dailyValues'ı Python'a gönderme
+        sendToPython();
+      }
+    };
+    xhr.open("GET", "dailyIntake.php", true);
+    xhr.send();
+});
+
+function sendToPython() {
+    fetch('http://127.0.0.1:5000/python_endpoint', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ dailyNeeds: dailyNeeds ,dailyValues: dailyValues })
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Python tarafından gelen yanıt:', data);
+      console.log('Python tarafından gelen mesaj:', data.message);
+    })
+    .catch(error => {
+      console.error('Hata:', error);
+    });
+}
+
+
+
+  
 </script>
 </body>
 </html>
